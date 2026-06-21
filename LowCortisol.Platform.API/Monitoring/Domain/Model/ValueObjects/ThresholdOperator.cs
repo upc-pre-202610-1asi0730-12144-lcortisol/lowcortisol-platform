5f289d5e67602b5ccc43ef354ed243b7fa64622c
Miguel Condori <miguelanibalcondori123@gmail.com>
2026-06-20 21:46:16 -0500

    feat(monitoring): add threshold domain

---
+++ LowCortisol.Platform.API/Monitoring/Domain/Model/ValueObjects/ThresholdOperator.cs
@@ -0,0 +1,9 @@
+namespace LowCortisol.Platform.API.Monitoring.Domain.Model.ValueObjects;
+
+public enum ThresholdOperator
+{
+    GreaterThan,
+    GreaterOrEqual,
+    LessThan,
+    LessOrEqual
+}
